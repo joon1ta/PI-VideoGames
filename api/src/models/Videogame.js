be7e540b0b1033ac3cd7c83ac18a59baf4e0bb82
joon1ta <armestojona@gmail.com>
2021-09-29 14:12:24 -0300

    Llamadas a Axios para traer los 100 juegos

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -29,16 +29,5 @@ module.exports = (sequelize) => {
       allowNull: false,
     }
   })
-  sequelize.define('genre', {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      primaryKey: true
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    }
-  });
+ 
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,24 +1,55 @@
-const { Router } = require('express');
-const URL = 'https://api.rawg.io/api/';
-const axios = require('axios');
+const { Router } = require("express");
+const URL = "https://api.rawg.io/api/";
+const axios = require("axios");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+require("dotenv").config();
 const { YOUR_API_KEY } = process.env;
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.get('/videogames', async (req, res) => {
-    try {
-        const games = await axios.get(`${URL}games?key=b43478ca85ed4cf7997dfde5f55531e1`);  
-        res.json(games.data.results)  
-    } catch(error) {
-        console.log(error)
-    }
-   
+router.get("/videogames", async (req, res) => {
+  
+  let { name } = req.query
+  if(!name) {
+    
+    const games1 = await axios.get(`${URL}games?key=${YOUR_API_KEY}`); // Consulta a la api para listar juegos por defualt nos trae 20
+    games_page2 = games1.data.next; // desde axios siempre accedemos a su respuesta con .data y el .result son los datos que queremos de la api
+    const games2 = await axios.get(games_page2)
+    games_page3 = games2.data.next
+    const games3 = await axios.get(games_page3)
+    games_page4 = games3.data.next
+    const games4 = await axios.get(games_page4)
+    games_page5 = games4.data.next
+    const games5 = await axios.get(games_page5)
+    let games = games1.data.results.concat(games2.data.results, games3.data.results, games4.data.results, games5.data.results);
+     games = games.map((game) => { // mapeamos los datos insertados en games_arr para tener un objeto con los datos especificos
+      return {
+        id: game.id,
+         name: game.name,
+         image: game.background_image,
+         description: game.description,
+         released: game.released,
+         rating: game.rating,
+         platforms: game.platforms,
+       genres: game.genres,
+         
+       };
+     });
+     res.json(games)
+}
+ 
 
 
-})
+  if(name) {
+      try {
+          
+      } catch (error) {
+        console.log(error);
+      }
+  }
+});
 
 module.exports = router;
